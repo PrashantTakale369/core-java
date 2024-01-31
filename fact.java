f5e1f75dec0ac4ddb2788dd19e48f7b9664ff807
Prashant Takale <147067738+PrashantTakale369@users.noreply.github.com>
2024-01-31 10:41:10 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -2,57 +2,58 @@ import java.util.*;
 
 public class fact{
 
-    //  ----Factorial of a given number :
+//  ----Factorial of a given number :
 
-    public static int factoral(int n) {
-        int fact1=1;
+public static int factoral(int n) {
+ int fact1=1;
 
-            for(int i=1;i<=n;i++){
-            fact1=i*fact1;
+  for(int i=1;i<=n;i++){
+  fact1=i*fact1;
              }
-            return fact1;
+return fact1;
 }
 
-     // ---find the Binomeal coefficent of NCR
+// ---find the Binomeal coefficent of NCR
 
 
-     public static int bincoeff(int n, int r) {
-        int fact_n = factoral(n);
-        int fact_r = factoral(r);
-        int fact_nmr = factoral(n-r);
+public static int bincoeff(int n, int r) {
+    int fact_n = factoral(n);
+    int fact_r = factoral(r);
+    int fact_nmr = factoral(n-r);
 
-        int bincoeff=fact_n/(fact_r*fact_nmr);
-        
-        return bincoeff;
+    int bincoeff=fact_n/(fact_r*fact_nmr);
+    
+    return bincoeff;
     
 }
 
-      // --- Concept of the Function Overloding
+// --- Concept of the Function Overloding
       
-      public static int multiplication(int a,int b) {
-        return a*b;
+public static int multiplication(int a,int b) {
+    return a*b;
         }
 
-        public static int multiplication(int a, int b,int c) {
-            return a*b*c;
+public static int multiplication(int a, int b,int c) {
+    return a*b*c;
  
 }
 
 // Check if a number is prime or not
 
-     public static int primeornot(int a) {
-        if(a%2==0){
-            System.out.print("Numer is not Prime : ");
-        }
-        else{
-            System.out.print("Numer is prime : ");
-        }
-        return a;
-        
+public static int primeornot(int a) {
+    if(a%2==0){
+        System.out.print("Numer is not Prime : ");
+ }
+else{
+System.out.print("Numer is prime : ");
+    }
+    return a;
+
 }
 
-  // Convert Bimery TO Decimal.
-  public static void BinToDeci(int binNum) {
+// Convert Bimery TO Decimal.
+    
+public static void BinToDeci(int binNum) {
     int myNum=binNum;
     int pow =0;
     int deci=0;
@@ -70,27 +71,26 @@ public class fact{
 // Print the Tabel  upto which you want 
 
 public static void tabel(int num) {
-
-  for(int i=1;i<=10;i++){
+for(int i=1;i<=10;i++){
     for(int j=1;j<=num;j++){
-      int result=i*j;
-      System.out.print(+ result);
+    int result=i*j;
+    System.out.print(+ result);
   }  
-  System.out.println();
-    }
+   System.out.println();
+ }
     
 }
     
 
-  public static void main(String[] args) {
-        Scanner Sc = new Scanner(System.in);
-        System.out.println("which no fact you want : ");
-        int fact=Sc.nextInt();
-        System.out.println(" binomia coiffecant of the give no : ");
-        int bin =Sc.nextInt();
-        System.out.println("this is the conecept function oveloding : ");   
-        int primeornot =Sc.nextInt();
-        System.out.println();     
+public static void main(String[] args) {
+    Scanner Sc = new Scanner(System.in);
+    System.out.println("which no fact you want : ");
+    int fact=Sc.nextInt();
+    System.out.println(" binomia coiffecant of the give no : ");
+    int bin =Sc.nextInt();
+    System.out.println("this is the conecept function oveloding : ");   
+    int primeornot =Sc.nextInt();
+    System.out.println();     
   
    
 
