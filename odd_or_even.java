85195154332aec229dc3b2ff6b22303f4bba6b8b
Prashant Takale <techprashant876@gmail.com>
2024-02-04 21:28:49 +0530

    odd or even

---
+++ odd_or_even.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class odd_or_even {
+ public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+    System.out.println("Enter the no whic you want to cheak the no is odd or even : ");
+    
+            int a = input.nextInt();
+    
+            if(a%2==0){
+                System.out.println("Your Number is Even");
+            }
+            else{
+                System.out.println("Your Number is odd : ");
+            }
+    
+        }
+        
+}
